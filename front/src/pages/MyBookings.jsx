407c8d627e73ef44bab347dc45c5324fba627a02
Rellinxe27 <bon19013@byui.edu>
2025-12-17 01:03:33 +0000

    updated

--- front/src/pages/MyBookings.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import BookingList from '../components/BookingList';
-
-export default function MyBookings({ user }) {
-  return <BookingList user={user} />;
-}
--- front/src/pages/ServicePage.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ServiceDetail from '../components/ServiceDetail';
-
-export default function ServicePage({ user }) {
-  return <ServiceDetail user={user} />;
-}
